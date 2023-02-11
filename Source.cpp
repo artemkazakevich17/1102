517455c1a102966affa27b0ca4d44b6edc36b95a
artemkazakevich17 <114008885+artemkazakevich17@users.noreply.github.com>
2023-02-11 19:31:06 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+#include "settings.h"
+#include "road.h"
+
+using namespace sf;
+
+int main()
+{
+
+	RenderWindow window(
+		VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+		WINDOW_TITLE,
+		Style::Titlebar | Style::Close
+	);
+	window.setPosition(Vector2i{ (1920 - (int)WINDOW_WIDTH) / 2, 0 });
+	window.setFramerateLimit(FPS);
+
+	RoadLayer grass1, grass2;
+	roadLayerInit(grass1, Vector2f{ 0.f,0.f }, "grass.jpg", 0.f);
+	roadLayerInit(grass2, Vector2f{ 0.f,-WINDOW_HEIGHT }, "grass.jpg", 0.f);
+
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			
+			if (event.type == Event::Closed)
+				
+				window.close();
+		}
+		//Update
+		roadLayerUpdate(grass1);
+		roadLayerUpdate(grass2);
+
+		window.clear();
+		roadLayeDraw(window, grass1);
+		roadLayeDraw(window, grass2);
+		window.display();
+	}
+
+	return 0;
+}
---
+++ player.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "settings.h"
+
+struct Player {
+	sf::Texture texture;
+	sf::Sprite sprite;
+	float speedx;
+};
+void playerInit(Player& player) {
+
+}
